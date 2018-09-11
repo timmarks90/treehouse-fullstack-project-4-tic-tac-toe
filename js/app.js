ffb5e26ec79576fa27d10288e231a5ff1c81ffcd
Tim Marks <timmarks90@gmail.com>
2018-09-11 15:37:41 -0700

    Added Tie Logic
    - End game tie logic
    - Added player name to end win screen

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ const nameInput = document.createElement('input');
 const endScreen = document.querySelector('#finish');
 endScreen.style.display = 'none';
 
+// Defining boxes to store values in once user clicks box
+xBox = [];
+oBox = [];
+
 nameInput.placeholder = 'What\'s your name?';
 nameInput.style.display = 'block';
 nameInput.style.margin = '50px auto';
@@ -10,8 +14,6 @@ nameInput.style.padding = '20px';
 nameInput.style.width = '250px';
 nameInput.style.borderRadius = '5px';
 nameInput.style.borderStyle = 'none';
-
-
 
 // Insert name input on welcome screen
 const startButton = document.querySelector('#start .button');
@@ -22,6 +24,7 @@ nameInput.focus();
 const name = () => {
     const board = document.querySelector('#board header');
     const playerNameDiv = document.createElement('div');
+    playerNameDiv.className = 'player-name';
     const playerNameTitle = document.createElement('h2');
     playerNameTitle.style.margin = '50px auto';
     playerNameTitle.style.textAlign = 'center';
@@ -32,13 +35,13 @@ const name = () => {
 }
 
 // On click of start button, hide intro screen
+const startScreen = document.querySelector('#start');
 startButton.addEventListener('click', () => {
     if(nameInput.value == '') {
         // Show error if user tried to start game without entering name
         nameInput.style.border = '2px solid rgb(255, 104, 104)';
     } else {
         name();
-        const startScreen = document.querySelector('#start');
         startScreen.style.display = 'none';
     }
 });
@@ -47,7 +50,6 @@ startButton.addEventListener('click', () => {
 nameInput.addEventListener("keydown", e => {
     if(e.keyCode == 13) {
         name();
-        const startScreen = document.querySelector('#start');
         startScreen.style.display = 'none';
     }
 })
@@ -103,10 +105,12 @@ function boxClick() {
         box[i].addEventListener('click', e => {
             if (player1.classList.contains('active') && (e.target.classList.contains('box-filled-1') == false && e.target.classList.contains('box-filled-2') == false)) {
                 e.target.className += ' box-filled-1';
+                oBox.push(e.target[i]);
                 endGame();
                 player2Turn();
             } else if (player2.classList.contains('active') && (e.target.classList.contains('box-filled-1') == false && e.target.classList.contains('box-filled-2') == false)) {
                 e.target.className += ' box-filled-2';
+                xBox.push(e.target[i]);
                 endGame();
                 player1Turn();
             }
@@ -127,38 +131,60 @@ const winningMoves = [
     [box[2], box[4], box[6]]
 ];
 
-
+// Declare winning player and show the proper winning page
 function endGame() {
     for (let i = 0; i < winningMoves.length; i++) {
         const oWins = winningMoves[i][0].classList.contains('box-filled-1') && winningMoves[i][1].classList.contains('box-filled-1') && winningMoves[i][2].classList.contains('box-filled-1');
         const xWins = winningMoves[i][0].classList.contains('box-filled-2') && winningMoves[i][1].classList.contains('box-filled-2') && winningMoves[i][2].classList.contains('box-filled-2');
-        const tied = ((winningMoves[i][0].classList.contains('box-filled-1') == false) && (winningMoves[i][1].classList.contains('box-filled-1') == false) && (winningMoves[i][2].classList.contains('box-filled-1') == false)) || ((winningMoves[i][0].classList.contains('box-filled-2') == false) && (winningMoves[i][1].classList.contains('box-filled-2') == false) && (winningMoves[i][2].classList.contains('box-filled-2') == false));
-        if(oWins == true || xWins == true) {
-            console.log('winner!');
-            winner('Winner');
-        } else if (tied == true) {
-            console.log('tied');
+        if(oWins == true) {
+            newGamePage(nameInput.value + ' Wins!', ' screen-win-one');
+        } else if (xWins == true) {
+            newGamePage('Winner', ' screen-win-two');
+        } else if ((oWins == false || xWins == false) && (xBox.length + oBox.length == 9)) {
+            if(endScreen.classList.contains('screen-win-tie')) {
+                break;
+            } else {
+                newGamePage('It\'s a Tie!', ' screen-win-tie');
+                console.log('tie');
+            }
         }
     }
 }
 
-function winner(message) {
+// Winning page for the player who won
+function newGamePage(message, winningPage) {
     endScreen.style.display = 'block';
-    endScreen.className += ' screen-win-one';
+    endScreen.className += winningPage;
     const winMessage = document.querySelector('.message');
     winMessage.innerHTML = message;
+    const newGameButton = document.querySelector('#finish .button');
+    // When user clicks New Game, show board and reset values to begin new game
+    newGameButton.addEventListener('click', () => {
+        endScreen.style.display = 'none';
+        resetGame();
+    })
 }
 
-if (player1) {
-}
-
-function xWins() {
-    if (winningMoves) {
-
+// Reset game to beginning default values
+function resetGame() {
+    nameInput.value = '';
+    const playerTitle = document.querySelector('.player-name');
+    if(endScreen.classList.contains('screen-win-one') || endScreen.classList.contains('screen-win-two') || endScreen.classList.contains('screen-win-tie')) {
+        endScreen.classList.remove('screen-win-one');
+        endScreen.classList.remove('screen-win-two');
+        endScreen.classList.remove('screen-win-tie');
     }
-    for (let i = 0; i < winningMoves.length; i++) {
-        if (winningMoves[i].classList.contains('box-filled-1') && winningMoves[i].classList.contains('box-filled-2') ) {
-            console.log('x wins!');
-        }
+    for (let i = 0; i < box.length; i++) {
+        box[i].classList.remove('box-filled-1');
+        box[i].classList.remove('box-filled-2');
     }
+    xBox = [];
+    oBox = [];
+    player1Turn();
+    nameInput.focus();
 }
+
+// startScreen.style.display = 'block';
+// if(playerTitle){
+//     playerTitle.remove();
+// }
